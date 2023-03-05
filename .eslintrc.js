073a9be0d2a77b4f95a8f4aaa60c6a9d067b03b3
Alexey Ryabov <lesha_12_01@mail.ru>
2023-03-05 18:26:31 +0300

    chore: setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,74 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:unicorn/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+        pathGroups: [{ pattern: '@/**', group: 'internal' }],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/newline-after-import': 'error',
+    'import/no-cycle': 'error',
+    'import/no-named-as-default-member': 'off',
+    'import/namespace': 'off',
+
+    'unicorn/no-null': 'off',
+    'unicorn/prefer-module': 'off',
+    'unicorn/consistent-destructuring': 'off',
+    'unicorn/prefer-number-properties': ['error', { checkInfinity: false }],
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          ref: false,
+          props: false,
+          args: false,
+          param: false,
+          params: false,
+          fn: false,
+          vars: false,
+        },
+      },
+    ],
+    'unicorn/no-negated-condition': 'off',
+
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'function-expression',
+      },
+    ],
+
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+      },
+    ],
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: { '{}': false },
+        extendDefaults: true,
+      },
+    ],
+  },
+};
