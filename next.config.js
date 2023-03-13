3b4838beb1acd3a2d23f89306bd96c1e71bc83be
Alexey Ryabov <lesha_12_01@mail.ru>
2023-03-13 23:11:40 +0300

    chore: enable React strict mode

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
